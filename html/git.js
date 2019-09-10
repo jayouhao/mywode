2591bdf823560745ab71a5c60fe7a59405b54120
home <2991944127@qq.com>
2019-09-10 21:21:40 +0800

    123

--- html/git.js
+++ html/git.js
@@ -1,9 +1,5 @@
-var num=0;
 var time=setInterval(function(){
-    num++;
-    sessionStorage.setItem("num123",num)
     if(num>20){
-        sessionStorage.removeItem("num123");
         localStorage.removeItem("islogins")
     }
 },3000)
